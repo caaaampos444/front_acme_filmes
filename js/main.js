d75e63876d75ad57abc49ee8e6c1ea3c83e14140
GUSTAVO DE CAMPOS <23111788@CFP127ALUNO.LOCAL>
2024-03-14 17:24:39 -0300

    tela detalhes do filme finalizada

--- js/main.js
+++ js/main.js
@@ -2,25 +2,30 @@
 
 import { getFilmes, getFilmeByID } from "./filmes.js"
 
-// function criarCard(filme){
-//     const card=document.createElement('div')
-//     const titulo=document.createElement('h2')
-//     titulo.textContent=filme.nome
-//     const texto=document.createElement('textarea')
-//     texto.textContent=filme.sinopse
-//     card.append(titulo, texto)
-
-//     return card
-// }
+function criarCard(filme){
+    const button=document.createElement('button')
+    button.classList.add('bg-transparent')
+    const card=document.createElement('div')
+    const poster=document.createElement('img')
+    poster.src=filme.foto_capa
+    poster.classList.add('w-mdivposterw','h-mdivposterh')
+    const titulo=document.createElement('h2')
+    titulo.textContent=filme.nome
+    titulo.classList.add('text-titulo','text-white','font-bold')
+    card.append(poster, titulo)
+    button.append(card)
+    return button
+}
 
 async function preencherContainer(){
-    // const container=document.querySelector('body')
+    const container=document.querySelector('main')
     const filmes=await getFilmes()
     filmes.forEach (filme=>{
-        // const card=criarCard(filme)
-        // container.appendChild(card)
+        const card=criarCard(filme)
+        container.appendChild(card)
         console.log(filme)
     })
 }
 
 preencherContainer()
+criarCard()
